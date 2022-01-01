f083da405f3dfad5e28e9a02029aa1c4890915be
David Mota <83174541+davidsilvamota@users.noreply.github.com>
2022-01-01 10:59:39 -0300

    commit initial

---
+++ src/components/Teste.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default class Teste extends React.Component {
+    render(): React.ReactNode {
+        return(
+        <>
+        <Text>Teste</Text>
+        </>
+        )
+    }
+}
